a831d2e26d0f9dadefcb991a65273b15be5a83c6
Peter Spier <spierp@gmail.com>
2012-10-16 15:25:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salish::Application.config.secret_token = 'df3661052a96e7fa78880ee6c703bea1662e1e2fa709dfbf48cc0cf95063369dd0fa090b7c3423f30098ca9b0e362656a35d4329b21b3c823cb413aa52e979de'
