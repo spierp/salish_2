02576e5d267bab1ab35e3242f04f9ffe2ccb241a
Peter Spier <spierp@gmail.com>
2012-10-16 18:32:42 -0700

    trying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,7 @@ Salish::Application.configure do
     :port                 => 587,
     :user_name            => 'salish.mailer@gmail.com',
     :password             => 's@lishMailer',
-#    :authentication       => 'plain',
-#    :enable_starttls_auto => true  
-}
+    :authentication       => 'plain',
+    :enable_starttls_auto => true }
     
 end
