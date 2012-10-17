cad6be9a15d647683c09037f8679b711ad8d8f8f
Peter Spier <spierp@gmail.com>
2012-10-17 16:06:04 -0700

    sidekiq web lockdown

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,8 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { :url => 'redis://redistogo:d40f9a0d470957623b7afa2ffce42fce@drum.redistogo.com:9998/', :namespace => 'radiant-tundra-7432.herokuapp.com', :size => 1 }
 end
+
+constraint = lambda { |request| request.env["warden"].authenticate? and request.env['warden'].user.admin? }
+constraints constraint do
+  mount Sidekiq::Web => '/sidekiq'
+end
