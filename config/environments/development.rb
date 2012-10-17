91542fd9812a3d827a7f2fd616e35882feb6f052
Peter Spier <spierp@gmail.com>
2012-10-16 17:26:51 -0700

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Salish::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #devise config settings
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Salish::Application.routes.draw do
 
+  devise_for :users
+
   root to: 'static_pages#home'
   
   match '/about', to: 'static_pages#about'
