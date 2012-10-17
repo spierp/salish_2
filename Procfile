8778140d3e2307486ce300ca3b7eb5b8328a353a
Peter Spier <spierp@gmail.com>
2012-10-17 13:09:22 -0700

    redis4

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-worker: bundle exec sidekiq
+worker: bundle exec sidekiq -e production -c 3
 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://redistogo:d40f9a0d470957623b7afa2ffce42fce@drum.redistogo.com:9998/', :namespace => 'radiant-tundra-7432.herokuapp.com' }
+  config.redis = { :url => 'redis://redistogo:d40f9a0d470957623b7afa2ffce42fce@drum.redistogo.com:9998/', :namespace => 'radiant-tundra-7432.herokuapp.com', :size => 7 }
 end
 
 Sidekiq.configure_client do |config|
