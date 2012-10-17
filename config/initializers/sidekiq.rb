aebc79f4a3ec2b6f89c876793b319a4a87d87f60
Peter Spier <spierp@gmail.com>
2012-10-17 12:51:40 -0700

    redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://redis.example.com:7372/12', :namespace => 'mynamespace' }
+  config.redis = { :url => 'redis://redistogo:d40f9a0d470957623b7afa2ffce42fce@drum.redistogo.com:9998/', :namespace => 'radiant-tundra-7432.herokuapp.com' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :url => 'redis://redistogo:d40f9a0d470957623b7afa2ffce42fce@drum.redistogo.com:9998/', :namespace => 'radiant-tundra-7432.herokuapp.com', :size => 1 }
 end
