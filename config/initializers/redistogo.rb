a70ec205e9fe0476aac3e14bf1eae86ca631decd
Peter Spier <spierp@gmail.com>
2013-01-15 17:05:53 -0800

    yeah?

--- config/initializers/redistogo.rb
+++ config/initializers/redistogo.rb
@@ -1,4 +1,4 @@
-ENV["REDISTOGO_URL"] = 'redis://redistogo:d40f9a0d470957623b7afa2ffce42fce@drum.redistogo.com:9998/'
+ENV["REDISTOGO_URL"] = 'redis://redistogo:7374996ddbb15ef97fd5f67ac87ad475@chubb.redistogo.com:9069/'
 
 #uri = URI.parse(ENV["REDISTOGO_URL"])
 #REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://redistogo:d40f9a0d470957623b7afa2ffce42fce@drum.redistogo.com:9998/', :namespace => 'radiant-tundra-7432.herokuapp.com', :size => 7 }
+  config.redis = { :url => 'redis://redistogo:7374996ddbb15ef97fd5f67ac87ad475@chubb.redistogo.com:9069/', :namespace => 'salish.herokuapp.com', :size => 7 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://redistogo:d40f9a0d470957623b7afa2ffce42fce@drum.redistogo.com:9998/', :namespace => 'radiant-tundra-7432.herokuapp.com', :size => 1 }
+  config.redis = { :url => 'redis://redistogo:7374996ddbb15ef97fd5f67ac87ad475@chubb.redistogo.com:9069/', :namespace => 'salish.herokuapp.com', :size => 1 }
 end
 
