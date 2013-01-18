eba7a69a761f916770bff69b37cf59b7b388d493
Peter Spier <spierp@gmail.com>
2013-01-18 09:02:06 -0800

    yeah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Salish::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => "salish.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://sali.sh" }
   
   #SMTP 
     config.action_mailer.delivery_method = :smtp
