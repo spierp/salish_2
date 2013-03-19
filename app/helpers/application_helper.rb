e76ed474a5b6ee92ac418454add987a7655056a5
Peter Spier <spierp@gmail.com>
2013-03-18 21:37:05 -0700

    expressions further

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,34 @@ module ApplicationHelper
     return options_html.join("\n")
   end
   
+  def expression_image(string)
+      exp = File.basename(string)
+      if exp == "expression1"
+        "expression1.jpg"
+      elsif exp == "expression2"
+        "expression2.jpg"
+      elsif exp == "expression3"
+        "expression3.jpg"
+      elsif exp == "expression4"
+        "expression4.jpg"
+      elsif exp == "expression5"
+        "expression5.jpg"
+      elsif exp == "expression6"
+        "expression6.jpg"
+      elsif exp == "expression7"
+        "expression7.jpg"
+      elsif exp == "expression8"
+        "expression8.jpg"
+      elsif exp == "expression9"
+        "expression9.jpg"
+      elsif exp == "expression10"
+        "expression10.jpg"
+      elsif exp == "expression11"
+        "expression11.jpg"        
+     else
+      "expression12.jpg"
+    end
+  end
+  
 end
+
